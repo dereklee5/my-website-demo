58c5693b8d3109ced1e08a7e3d0b1f885d924f01
dereklee5 <dlee43@uw.edu>
2023-11-24 18:52:48 -0800

    navbar global component

--- app/page.js
+++ app/page.js
@@ -1,30 +1,10 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Navbar />
-      <Navbar />
-      <Navbar />
-      <Navbar />
-      <Navbar />
+      <p>This is the home page!</p>
     </div>
-  );
-}
-
-function Navbar() {
-  return (
-    <nav className="border-b border-gray-800 sticky top-0 bg-gray-900 text-gray-100 z-10">
-    <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
-      <a href="/" className="font-medium text-lg md:hover:underline">My Website</a>
-      <ul className = "flex items-center justify-end space-x-4 text-sm font-medium">
-        <li className="md:hover:underline">
-          <a href="/blog">Blog</a>
-        </li>
-        <li className="md:hover:underline">
-          <a href="/photos">Photos</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
   );
 }

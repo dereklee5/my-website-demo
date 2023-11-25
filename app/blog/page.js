a1a2b6654d58c9da2eddd06b94a0b621924da53a
dereklee5 <dlee43@uw.edu>
2023-11-24 22:59:23 -0800

    syncing blog studio to blog posts

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,5 +1,29 @@
 import Navbar from "../components/Navbar";
+import BlogPostCard from "./components/BlogPostCard";
+import { client } from "@/sanity/lib/client";
 
-export default function Blog() {
-  return <p>This is the blog!</p>;
+export default async function Blog() {
+  const posts = await getBlogPosts();
+  return (
+      <div className="py-8 max-w-7xl px-4 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {posts.map((post) => (
+            <BlogPostCard key={post.slug} post={post} />
+          ))}
+        </div>
+      </div>
+  );
+}
+
+async function getBlogPosts() {
+  const query = `*[_type == "blogPost"] | order(date desc) {
+    title,
+    description,
+    date,
+    "slug":slug.current,
+    image
+  }`;
+
+  const posts = await client.fetch(query);
+  return posts;
 }
